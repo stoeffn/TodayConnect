b88df1ad4b457ed77193e2303651d2b77c27ca9c
stoeffn <13511737+stoeffn@users.noreply.github.com>
2018-05-25 20:14:55 +0200

    :tada: Generate macOS app project

---
+++ TodayConnect/AppDelegate.swift
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.swift
+//  TodayConnect
+//
+//  Created by Steffen Ryll on 25.05.18.
+//  Copyright © 2018 Steffen Ryll. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+final class AppDelegate: NSObject {}
+
+extension AppDelegate: NSApplicationDelegate {}
---
+++ TodayConnect/ViewController.swift
@@ -0,0 +1,11 @@
+//
+//  ViewController.swift
+//  TodayConnect
+//
+//  Created by Steffen Ryll on 25.05.18.
+//  Copyright © 2018 Steffen Ryll. All rights reserved.
+//
+
+import Cocoa
+
+final class ViewController: NSViewController {}
