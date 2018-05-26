fb1fa832e7634a9a61fb50b9b03911c91366e993
stoeffn <13511737+stoeffn@users.noreply.github.com>
2018-05-26 11:52:53 +0200

    :sparkles: Add Olympus api client

---
+++ TodayConnectKit/Olympus/OlympusApiRoutes.swift
@@ -0,0 +1,43 @@
+//
+//  TodayConnect
+//
+//  Copyright © 2018 Steffen Ryll
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation
+//  files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy,
+//  modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the
+//  Software is furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in all copies or substantial portions of the
+//  Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+//  WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+//  COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+//  ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+
+enum OlympusApiRoutes: ApiRoutes {
+    case appConfiguration(host: String)
+
+    var path: String {
+        switch self {
+        case .appConfiguration:
+            return "app/config"
+        }
+    }
+
+    var parameters: [URLQueryItem] {
+        switch self {
+        case let .appConfiguration(host):
+            return [URLQueryItem(name: "host", value: host)]
+        }
+    }
+
+    var responseType: Decodable.Type? {
+        switch self {
+        case .appConfiguration:
+            return AppConfigResponse.self
+        }
+    }
+}
